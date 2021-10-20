ff76d16e651cc0c84e89a3ff29682cbbdd9f4bff
gifyic <81018600+gifyic@users.noreply.github.com>
2021-10-20 10:56:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "更新源，以及安装所需环境" && sudo apt update && sudo apt-get install -y openssl ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python  m4 gcc libtool zlib1g-dev flex gcc-aarch64-linux-gnu libssl-dev
-git clone --recurse-submodules https://service-85nm4dou-1306003300.hk.apigw.tencentcs.com/release/start-test-1632887300/Candy_clang-20201120.tar.zst --depth=1
+git clone --recurse-submodules https://github.com/gifyic/paper-xm-msm8998-kernel.git --depth=1
 wget https://service-85nm4dou-1306003300.hk.apigw.tencentcs.com/release/start-test-1632887300/Candy_clang-20201120.tar.zst
 echo "安装zstd支持" && sudo apt-get install -y zstd
 echo "开始解压ClangBuiltLinux Clang" && mkdir cbl && tar -I zstd -xvf Candy_clang-20201120.tar.zst -C cbl
