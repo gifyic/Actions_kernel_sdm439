c79f5bbd00a4a16b77663063ae4eb6ea0ac79dcc
gifyic <81018600+gifyic@users.noreply.github.com>
2021-10-30 22:59:53 +0800

    There is no use in removing judgments

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-echo "更新源，以及安装所需环境" && sudo apt update && sudo apt-get install -y openssl ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python  m4 gcc libtool zlib1g-dev flex gcc-aarch64-linux-gnu libssl-dev
-if [ -d $GITHUB_WORKSPACE/kernel_cherry_sdm439 ];then
-   echo "内核源码已存在，跳过下载"
-else 
-   echo "内核源码未下载，开始下载" && git clone https://github.com/gifyic/kernel_cherry_sdm439.git --depth=1
-fi
-if [ -d $GITHUB_WORKSPACE/Candy_clang-20201120.tar.zst ];then
-   echo "ClangBuiltLinux Clang 已下载，跳过下载"
-else
-   echo "ClangBuiltLinux Clang 未下载，开始下载" && wget https://morning-grape-butterkase.glitch.me/OnedriveXbot/Candy_clang-20201120.tar.zst
-fi
-echo "安装zstd支持" && sudo apt-get install -y zstd
-echo "开始解压ClangBuiltLinux Clang" && mkdir cbl && tar -I zstd -xvf Candy_clang-20201120.tar.zst -C cbl
-echo "进入内核源码根目录"
+sudo apt update && sudo apt-get install -y openssl ccache bc bash git-core gnupg build-essential zip curl make automake autogen autoconf autotools-dev libtool shtool python  m4 gcc libtool zlib1g-dev flex gcc-aarch64-linux-gnu libssl-dev
+git clone https://github.com/gifyic/kernel_cherry_sdm439.git --depth=1
+wget https://morning-grape-butterkase.glitch.me/OnedriveXbot/Candy_clang-20201120.tar.zst
+sudo apt-get install -y zstd
+mkdir cbl && tar -I zstd -xvf Candy_clang-20201120.tar.zst -C cbl
 cd $GITHUB_WORKSPACE/kernel_cherry_sdm439
 args="-j$(nproc --all) \
 	O=out \
